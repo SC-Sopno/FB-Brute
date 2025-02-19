fd3d7ab5372c79ae00806b939b0acbdcdf47a7b8
Sopno Chakrobortty <sopnochakroborty5@gmail.com>
2025-02-19 21:49:19 +0600

    Add files via upload

---
+++ cadow/banner.py
@@ -0,0 +1,47 @@
+import os
+import time as cabdulahi
+
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk)) 
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk)) 
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk)) 
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk)) 
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk)) 
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk)) 
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk)) 
+def prBlack(skk): print("\033[98m {}\033[00m" .format(skk)) 
+
+green = '\033[32;1m'
+
+gta = '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+
+white = '\033[1;37m' # White 
+red = '\033[31m' # red
+orange = '\033[33m' # orange
+blue = '\033[34m' # blue
+p  = '\033[35m' # purple
+C  = '\033[36m' # cyan
+
+
+class banner:
+    def __init__(self):
+        print C+orange+"""
+  __ _     _
+ / _| |__ | |__    _
+ 『S』『C』
+        『S』『o』『p』『n』『o』
+        . . . . . . . . . . . . 
+        
+                        Facebook Bruteforce
+                           Faster 1.0 kps
+                           Added More Info
+                    [SC_Sopno Facebook Bruteforce Tool]
+                        [@Created By SC_Sopno] """+C+p+"""
+                  Telegram Chennal: https://t.me/OSS_Cyber_Team
+                Facebook Page: https://www.facebook.com/OSSCyberTeam"""
+        cabdulahi.sleep(2)
+        print
+        
+        
+        
+        
+       
